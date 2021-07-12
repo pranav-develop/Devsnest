1b3a282bd60c9d8cb5e23aa0b3511923e3c90a80
pranav-develop <pranavschiller@gmail.com>
2021-07-13 00:25:00 +0530

    assignment 18 completed

---
+++ assignment-18/src/App.js
@@ -0,0 +1,13 @@
+//jshint esversion: 8
+import React from "react";
+import MainBox from "./components/MainBox";
+
+function App() {
+    return (
+        <React.Fragment>
+            <MainBox />
+        </React.Fragment>
+    );
+}
+
+export default App;
